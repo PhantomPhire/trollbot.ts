341213111c72785d5308a040e0b380727bfb948b
Jessie Monterroso <montj@fordav.com>
2019-02-23 22:39:12 -0600

    forgot to stage something...I'm new at this

--- src/TrollBotManager.ts
+++ src/TrollBotManager.ts
@@ -3,6 +3,7 @@ import {CommandoClient} from "discord.js-commando";
 import {BotManager, GuildAudioPlayer, SoundFileManager} from "../DiscordBotUtils";
 import {RoastManager} from "./utility/RoastManager";
 import {Trolling} from "./utility/Trolling";
+import {SuggestionsManager} from "./utility/SuggestionsManager";
 
 /**
  * A wrapper for the TrollBot, managing its events and internals
@@ -14,6 +15,7 @@ export class TrollBotManager extends BotManager {
     constructor() {
         super(new CommandoClient({ commandPrefix: (<any>config).prefix, owner: (<any>config).owner }));
         RoastManager.initialize();
+        SuggestionsManager.initialize();
         let soundConfig = (<any>config).soundPath as string | undefined;
         if (soundConfig !== undefined) {
             SoundFileManager.initialize((<any>config).soundPath);
@@ -32,6 +34,7 @@ export class TrollBotManager extends BotManager {
         this._bot.registry.registerGroup("trolling", "Trolling");
         this._bot.registry.registerGroup("roasts", "Roasts");
         this._bot.registry.registerGroup("sounds", "Sounds");
+        this._bot.registry.registerGroup("suggestions", "Suggestions");
         this._bot.registry.registerDefaults();
         this._bot.registry.registerCommandsIn(__dirname + "/commands");
     }
